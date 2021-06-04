68ec7c41cf68fe1f06bed569ca76daa8eb6a1a06
David Lapointe <info@davidweb.ca>
2021-06-04 15:59:57 -0400

    Fix vendor build?

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,7 @@ require('@tinypixelco/laravel-mix-wp-blocks');
 mix
   .setPublicPath('./dist')
   .blocks('src/scripts/app.js', 'scripts')
+  .autoload({ jquery: ['$', 'window.jQuery'] })
   .extract()
   .sourceMaps()
   .version()
